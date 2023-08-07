abbd803bb1f135a2ef8a0ad4b08d19942cc7d703
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2023-08-07 11:16:16 -0400

    allow d-tree-testing from the haskell exe.

--- app/DTreeData.hs
+++ app/DTreeData.hs
@@ -3,7 +3,22 @@ module DTreeData where
 import Data.Maybe (fromMaybe)
 import qualified Data.Set as Set
 import Data.Word (Word64)
-import Options.Applicative ((<**>), argument, auto, help, helper, info, long, metavar, Parser, progDesc, short, strOption, value, execParser)
+import Options.Applicative ( (<**>)
+                           , argument
+                           , auto
+                           , help
+                           , helper
+                           , info
+                           , long
+                           , metavar
+                           , Parser
+                           , progDesc
+                           , short
+                           , strOption
+                           , switch
+                           , value
+                           , execParser
+                           )
 import Text.Parsec.String (parseFromFile)
 
 import Choreography hiding (source, value)
@@ -11,6 +26,7 @@ import Utils
 
 data Arguments = Arguments { iters :: IterConfig
                            , source :: Maybe FilePath
+                           , performTest :: Bool
                            }
 
 argParser :: Parser Arguments
@@ -20,13 +36,17 @@ argParser = do
     testingN <- argument auto (     metavar "TESTING"    <> help "How many rows of data to test the decision trees on.")
     source <- strOption (           long "file"          <> short 'f' <> metavar "FILENAME"
                                                          <> help "File to read from instead of stdIn." <> value "")
+    performTest <- switch (         short 'p'            <> long "perform-test"
+                                                         <> help ("INSTEAD of printing the data to stdOut, send it directly to python,"
+                                                                  ++ " and print the p-value."))
     return Arguments{iters = IterConfig {iterations, trainingN, testingN },
-                     source = case source of [] -> Nothing; _ -> Just source}
+                     source = case source of [] -> Nothing; _ -> Just source,
+                     performTest}
 
 main :: IO ()
 main = do
   let corruption = Parties $ Set.fromList [corrupt, p1]
-  Arguments{iters, source} <- execParser $
+  Arguments{iters, source, performTest} <- execParser $
       info (argParser <**> helper) (progDesc $ unlines [
         "Generate ITERATIONS * (TRAINING + TESTING) rows of trace data for consumption by the d-tree system.",
         "assumes " ++ pretty corruption ++ " are corrupt."])
@@ -34,5 +54,7 @@ main = do
   -- https://unix.stackexchange.com/questions/483573/a-command-wants-file-paths-how-can-i-give-it-stdin-for-the-infile-and-stdout
   program' <- parseFromFile (changeState (const ()) (const mempty) programParser) $ fromMaybe "/dev/stdin" source
   let program = either error id $ either (error . show) id $ validate mempty <$> program'
-  printParallelized @Word64 iters program corruption
+  if performTest
+    then experiment_ @Word64 iters program corruption >>= print
+    else printParallelized @Word64 iters program corruption
 
