c30e13d5d9ec7d61ae9fe8340f01ebab3414bfbc
Joe Near <jnear@uvm.edu>
2023-09-13 06:43:15 -0400

    tweaks to circuit experiment

--- python/circuit_experiment.py
+++ python/circuit_experiment.py
@@ -26,8 +26,6 @@ def run_experiment(cho_file, iters, train, test, results_filename):
     with open('stderr.log', "r") as f:
         data_time = float(f.read().replace('\'', ''))
 
-    print('time to generate data:', data_time)
-
     # run d-trees
     stdout_log = open('stdout.log', "w")
     stderr_log = open('stderr.log', "w")
@@ -47,22 +45,20 @@ def run_experiment(cho_file, iters, train, test, results_filename):
     with open('stderr.log', "r") as f:
         d_tree_time = float(f.read().replace('\'', ''))
 
-    print('time to run d_trees:', d_tree_time)
-    print('p-val:', pval)
-
     with open(results_filename, 'a') as f:
-        f.write(f'{iters},{train},{test},{pval},{data_time},{d_tree_time}\n')
+        line = f'{iters},{train},{test},{pval},{data_time},{d_tree_time}\n'
+        f.write(line)
 
 
 iters = [64, 128]
 trains = [2**i for i in range(7, 11)]
 
 cho_names = [
-'adder64_beaver.cho',
+#'adder64_beaver.cho',
 'adder64_gmw.cho',
-'aes_128_beaver.cho',
+#'aes_128_beaver.cho',
 'aes_128_gmw.cho',
-'divide64_beaver.cho',
+#'divide64_beaver.cho',
 'divide64_gmw.cho',
 'FP-add_beaver.cho',
 'FP-add_gmw.cho',
@@ -76,6 +72,7 @@ cho_names = [
 'sha256_gmw.cho']
 
 for cho_name in cho_names:
+    print('Running on:', cho_name)
     dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
     filename = f'results/circuit_results_{cho_name}_{dt_string}.csv'
     with open(filename, 'w') as f:
@@ -85,4 +82,4 @@ for cho_name in cho_names:
         for train in trains:
             print(iters, train, int(train/2))
 
-            run_experiment(cho_name, iters, train, int(train/2), filename)
+            run_experiment('circuits/' + cho_name, iters, train, int(train/2), filename)
