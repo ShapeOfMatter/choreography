31f81e8f5d511469b253372ba4885d9694c9b491
Joe Near <jnear@uvm.edu>
2023-09-17 18:53:59 -0400

    update circuit experiment file to perform breakages

--- python/circuit_experiment.py
+++ python/circuit_experiment.py
@@ -5,8 +5,21 @@ from datetime import datetime
 
 now = datetime.now()
 
+def gen_circuit_file(circuit_file, cho_file, protocol_type, config):
+    bias_sharing, bias_and, accidental_secret, accidental_gate = config
 
-def run_experiment(cho_file, iters, train, test, results_filename):
+    circuit_filename = 'circuits/' + circuit_file
+
+    p1 = subprocess.Popen(['python', f'python/{protocol_type}_circuit_generator.py',
+                           circuit_filename, cho_file,
+                           '--bias_sharing', str(bias_sharing),
+                           '--bias_and', str(bias_and),
+                           '--accidental_secret', str(accidental_secret),
+                           '--accidental_gate', str(accidental_gate)])
+    p1.wait()
+
+def run_experiment(cho_filename, iters, train, test, results_filename,
+                   protocol_type, config, circuit_name):
     # generate data
     data_csv = open('data.csv', "w")
     stderr_log = open('stderr.log', "w")
@@ -14,8 +27,6 @@ def run_experiment(cho_file, iters, train, test, results_filename):
     iters = str(iters)
     train = str(train)
     test = str(test)
-
-    cho_filename = 'circuits/' + cho_file
 
     p1 = subprocess.Popen(['/usr/bin/time', '-f', '\'%e\'', 'cabal', 'exec', 'd-tree-data', '--',
                            iters, train, test, '-f', cho_filename],
@@ -47,8 +58,9 @@ def run_experiment(cho_file, iters, train, test, results_filename):
     with open('stderr.log', "r") as f:
         d_tree_time = float(f.read().replace('\'', ''))
 
+    config_str = ','.join([str(x) for x in config])
     with open(results_filename, 'a') as f:
-        line = f'{cho_file},{iters},{train},{test},{pval},{data_time},{d_tree_time}\n'
+        line = f'{circuit_name},{protocol_type},{iters},{train},{test},{pval},{data_time},{d_tree_time},{config_str}\n'
         print(line)
         f.write(line)
 
@@ -56,37 +68,45 @@ def run_experiment(cho_file, iters, train, test, results_filename):
 iters = [64, 128]
 trains = [2**i for i in range(7, 11)]
 
-cho_names = [
-'adder64_beaver.cho',
-'adder64_gmw.cho',
-'aes_128_beaver.cho',
-'aes_128_gmw.cho',
-'divide64_beaver.cho',
-'divide64_gmw.cho',
-'FP-add_beaver.cho',
-'FP-add_gmw.cho',
-'FP-div_beaver.cho',
-'FP-div_gmw.cho',
-'FP-mul_beaver.cho',
-'FP-mul_gmw.cho',
-'mult64_beaver.cho',
-'mult64_gmw.cho',
-'sha256_beaver.cho',
-'sha256_gmw.cho']
+circuit_names = [
+    'adder64.txt',
+    'mult64.txt',
+    'aes_128.txt',
+    'divide64.txt',
+    'FP-add.txt',
+    'FP-mul.txt',
+    'sha256.txt',
+    'FP-div.txt']
+
+protocol_types = ['gmw', 'beaver']
+
 
 dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
 filename = f'results/circuit_results_{dt_string}.csv'
 with open(filename, 'w') as f:
-    f.write('circuit,iters,train_size,test_size,pval,data_time,d_tree_time\n')
+    f.write('circuit,protocol,iters,train_size,test_size,pval,data_time,d_tree_time,bias_sharing,bias_and,accidental_secret,accidental_gate\n')
 
 TRIALS = 3
 
-for cho_name in cho_names:
-    print('Running on:', cho_name)
+configs = [
+    (0, 0, 0, 0), # secure
+]
+configs += [(i, 0, 0, 0) for i in range(1, 4)]
+configs += [(0, i, 0, 0) for i in range(1, 4)]
+configs += [(0, 0, 1/i, 0) for i in range(2, 5)]
+configs += [(0, 0, 0, 1/i) for i in range(2, 5)]
 
-    for i in iters:
-        for train in trains:
-            for _ in range(TRIALS):
-                #print(i, train, int(train/2))
+cho_file = 'tmp.cho'
+for circuit_name in circuit_names:
+    print('Running on:', circuit_name)
+    for protocol_type in protocol_types:
+        for c in configs:
+            gen_circuit_file(circuit_name, cho_file, protocol_type, c)
 
-                run_experiment(cho_name, i, train, int(train/2), filename)
+            for i in iters:
+                for train in trains:
+                    for _ in range(TRIALS):
+                        print(i, train, int(train/2))
+
+                        run_experiment(cho_file, i, train, int(train/2), filename,
+                                       protocol_type, c, circuit_name)
